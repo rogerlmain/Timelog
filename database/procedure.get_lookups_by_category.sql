73ac0416872ff3b9cc1be1f8c61282bde440021a
Roger L. Main <roger.main@rexthestrange.com>
2022-04-02 07:08:09 -0600

    Added "active" field to lookups. Set embargoed countries to inactive.

--- database/procedure.get_lookups_by_category.sql
+++ database/procedure.get_lookups_by_category.sql
@@ -15,7 +15,8 @@ create procedure get_lookups_by_category (category_id integer) begin
 	from
 		lookups
 	where
-		lookups.category_id = category_id;
+		(lookups.category_id = category_id) and
+        (lookups.active);
         
 end??
 
