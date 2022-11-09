ec9e2ac4dc4a5cb0f2f2e951c0feae7841135d32
rexthestrange <roger.main@rexthestrange.com>
2022-11-09 14:57:24 -0700

    #105 Finish Teamster report

--- client/pages/reports.jsx
+++ client/pages/reports.jsx
@@ -473,24 +473,25 @@ export default class ReportsPage extends BaseControl {
 	}// show_totals;
 
 
-	timebar (time) {
+	timebar (time, maximum_hours) {
 
-		const minutes_per_hour = 1440;
-		const seconds_per_minute = 60;
+		const color_spread = 240;
 
-		let timestamp = new Date (time);
-		let elapsed_time = Date.timespan (timestamp.elapsed ());
+		let maximum_minutes = maximum_hours * 60;
+ 		let total_minutes = Math.floor (new Date (time).elapsed () / (Date.coefficient.minute * 1000));
 
-		let total_minutes = (elapsed_time.days * 24 * minutes_per_hour) + (elapsed_time.hours * minutes_per_hour) + elapsed_time.mins;
-		let time_width = ((total_minutes > (maximum_hours * seconds_per_minute)) ? minutes_per_hour : total_minutes);
+		let overtime = total_minutes > maximum_minutes;
+		let width_percentage = total_minutes / maximum_minutes;
 
-		let overtime = total_minutes > (maximum_hours * minutes_per_hour);
+		let red = (width_percentage >= 0.5) ? color_spread : color_spread * (width_percentage * 2);
+		let green = (width_percentage <= 0.5) ? color_spread : color_spread * (1 - ((width_percentage - 0.5) * 2));
 
-		let time_color = (overtime ? "#F00" : ((time_width > (maximum_session * seconds_per_minute)) ? "#880" : "#080"));
+		let time_color = `rgb(${red}, ${green}, 0)`;
 
-		return <div className="timebar">
-			<div className="fully-centered" style={{ 
-				width: `${Math.round (time_width / (maximum_hours * minutes_per_hour)) * 100}%`, 
+		return <div className="timebar" style={{ height: "0.5em" }}>
+			<div style={{
+				width: `${width_percentage * 100}%`, 
+				height: "100%",
 				backgroundColor: time_color,
 			}}>{overtime ? "OVERTIME" : blank}</div>
 		</div>
@@ -510,9 +511,10 @@ export default class ReportsPage extends BaseControl {
 		result.push (<div key={0} className="report-title">
 			<div key={1}>Name</div>
 			<div key={2}>Client</div>
-			<div key={3}>Notes</div>
-			<div key={4}>Start Time</div>
-			<div style={{ border: "none", backgroundColor: "var(--background-color)" }} />
+			<div key={3}>Project</div>
+			<div key={4}>Notes</div>
+			<div key={5}>Start Time</div>
+			<div key={6} style={{ border: "none", backgroundColor: "var(--background-color)" }} />
 		</div>);
 
 		for (let teamster of this.state.teamsters) {
@@ -523,9 +525,19 @@ export default class ReportsPage extends BaseControl {
 			result.push (<div className="report-row" key={teamster.log_id}>
 				<div key={1} title={teamster_name}>{teamster_name}</div>
 				<div key={2} title={teamster.client_name}>{teamster.client_name}</div>
-				<div key={3} title={teamster.notes}>{teamster.notes}</div>
-				<div key={4} title={start_time}>{start_time}</div>
-				<div key={5} style={{ border: "none" }}>{this.timebar (teamster.start_time)}</div>
+				<div key={3} title={teamster.project_name}>{teamster.project_name}</div>
+				<div key={4} title={teamster.notes}>{teamster.notes}</div>
+				<div key={5} title={start_time}>{start_time}</div>
+				<div key={6} style={{ border: "none" }}>
+					<div className="report-indicator">
+
+						{this.timebar (teamster.start_time, maximum_hours)}
+						{this.timebar (teamster.start_time, maximum_session)}
+
+						{isset (teamster.project_estimate) && this.timebar (teamster.total_time, teamster.project_estimate ?? 100)}
+
+					</div>					
+				</div>
 			</div>);
 
 		}// for;
